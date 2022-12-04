8790b316f2ced3367396b44a892562d9d72c0095
Michael Boyles <michael@boyl.es>
2022-12-04 13:47:13 +0000

    fix: highlighting when WK uses special placeholder syntax

--- src/components/HighlightedSentence.tsx
+++ src/components/HighlightedSentence.tsx
@@ -1,9 +1,10 @@
 export function HighlightedSentence(props: {word: string, sentence: string}) {
-    const regex = new RegExp('(' + props.word + ')', "g");
+    const word = props.word.startsWith('〜') ? props.word.substring(1) : props.word;
+    const regex = new RegExp('(' + word + ')', "g");
     const fragments = props.sentence.split(regex);
     return (<>{
         fragments.map(
-            (fragment, i)=> <span key={i} className={fragment === props.word ? 'highlight' : ''}>{fragment}</span>
+            (fragment, i)=> <span key={i} className={fragment === word ? 'highlight' : ''}>{fragment}</span>
         )
     }</>);
 }
